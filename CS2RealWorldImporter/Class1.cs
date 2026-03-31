d963d4c379032f837a6f697cace045aeaacad264
gelovcd-crypto <gelovcd@gmail.com>
2026-03-31 14:22:46 -0500

    Create Class1.cs

---
+++ CS2RealWorldImporter/Class1.cs
@@ -0,0 +1,9 @@
+using BepInEx;
+
+namespace CS2RealWorldImporter
+{
+    [BepInPlugin("com.vcd.cs2realworldimporter", "CS2 Real World Importer", "1.0.0")]
+    public class Plugin : BaseUnityPlugin
+    {
+    }
+}
